68dc691ab9d11084eda1df40d5158f62e7788f86
shkh07 <shkh@ucdavis.edu>
2025-02-06 11:58:35 -0800

    update

--- 22fibonacci.py
+++ 22fibonacci.py
@@ -1,6 +1,10 @@
 def fibonacci(n):
-	fib_sequence = (0, 1)
-	for i in range(2, n):
-		fib_sequence = fib_sequence + (fib_sequence[-2] + fib_sequence[-1],)
-	return fib_sequence
-print(fibonacci(10))
+	a, b = 0, 1
+	
+	for i in range(n):
+		print(a)
+		a, b = b, a + b
+		
+fibonacci(10)
+
+
--- 24savingthrows.py
+++ 24savingthrows.py
@@ -1,26 +1,26 @@
 import random
 
-def saving_throws(trials): # function for simulating throws
-	dc5 = 0 # successes for dc5
-	dc10 = 0
-	dc15 = 0
-	failures = 0 # count rolls below dc 5
-
-	for i in range(trials):
-		roll = random.randint(1, 20)
-		
-		# gonna look for the highest dc first
-		if roll >= 5:
-			dc5 += 1
-		elif roll >= 10:
-			dc10 += 1
-		elif roll >= 15:
-			dc15 +1
-		else:
-			failures += 1
-	print(dc5 / trials)
-	print(dc10 / trials)
-	print(dc15 / trials)
-	print(failures / trials)
-print(saving_throws(100000)) # running the program with 100000 trials
+def saving_throws(dc, mode):
+	roll1 = random.randint(1, 20)
+	roll2 = random.randint(1, 20)
 	
+	if mode == 'advantage':
+		roll = (roll1, roll2)
+		if roll1 > roll2:
+			roll = roll1
+		else:
+			roll = roll2
+	elif mode == 'disadvantage':
+		roll = (roll1, roll2)
+		if roll1 < roll2:
+			roll = roll1
+		else:
+			roll = roll2
+	else:
+		roll = roll1
+	
+	if roll >= dc: return 'success'
+	else: return 'no success'
+
+print(saving_throws(10, 'disadvantage'))
+print(saving_throws(21, 'disadvantage'))
