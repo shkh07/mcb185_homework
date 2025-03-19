820fbe6486830ea27825ad6365fe83fd45b7f9ed
shkh07 <shkh@ucdavis.edu>
2025-03-18 22:17:00 -0700

    new test 5

--- 5test.py
+++ 5test.py
@@ -45,60 +45,6 @@ for aa, p in count.items():
 	print(f'{code} {100*p:.2f}', end='\t')
 	if x % 4 == 0: print()
 	x += 1
-	
-"""
-
-aa_table = list(count.keys())
-for i in range(0, len(aa_table), 4):
-	for j in range(i, i+4):
-		if j < len(aa_table):
-			aa, freq = aa_table[j]
-			if aa in three_etter_code:
-				code = three_letter_code[aa]
-			else:
-				code = 'N/A'
-			print(f"{code:<20} {freq*100}", end="\t")
-		print()
-	
-
-for aa, freq in count.items():
-	count[aa] = freq / total
-	aa_table = []
-	aa_table.append([three_letter_code[aa], count[aa]])
 
 
-print("AA\tFrequency\tAA\tFrequency\tAA\tFrequency\tAA\tFrequency")
-
-for i in range(0, len(aa_table), 4):
-	rows = [aa_table[i:i+4]
-
-
-
-
-
-
-# count the frequencies
-
-frequencies = aa_freq(seq)
-aa_table = []
-for aa, freq in 
-
-# wanna now make a dictionary with the 20 aa
-aa_count = {}
-for aa in 'ARNDCEQGHILKMFPSTWYV'
-	aa_count[aa] = 0
-total_aa_count = 0
-
-# now lets like look for the amino acids in the sequence kinda
-
-for aa, freq in count.items():
-	if aa in aa_count:
-		aa_count[aa] += freq
-		'''
-	'''
-	for row in rows:
-	for aa in row:
-		print(aa[0], aa[1], end="\t")
-	print()
-"""
 		
